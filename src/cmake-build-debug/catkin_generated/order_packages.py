a4eaa0c68649c34f6861394ff7d4c363505a8f3e
YZT1997 <yangzt_0943@163.com>
2020-12-01 21:50:50 +0800

    update

--- src/cmake-build-debug/catkin_generated/order_packages.py
+++ src/cmake-build-debug/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = '/home/yangzt/yangzt_ws/src'
 whitelisted_packages = ''.split(';') if '' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/yangzt/amz_driverless/fsd_skeleton/devel;/home/yangzt/baxtersim_ws/devel;/home/yangzt/baxter_ws/devel;/home/yangzt/yangzt_ws/devel;/opt/ros/kinetic'.split(';') if '/home/yangzt/amz_driverless/fsd_skeleton/devel;/home/yangzt/baxtersim_ws/devel;/home/yangzt/baxter_ws/devel;/home/yangzt/yangzt_ws/devel;/opt/ros/kinetic' != '' else []
+underlay_workspaces = '/home/yangzt/yammar_ws/devel;/home/yangzt/amz_driverless/fsd_skeleton/devel;/home/yangzt/baxtersim_ws/devel;/home/yangzt/baxter_ws/devel;/home/yangzt/yangzt_ws/devel;/opt/ros/kinetic'.split(';') if '/home/yangzt/yammar_ws/devel;/home/yangzt/amz_driverless/fsd_skeleton/devel;/home/yangzt/baxtersim_ws/devel;/home/yangzt/baxter_ws/devel;/home/yangzt/yangzt_ws/devel;/opt/ros/kinetic' != '' else []
--- src/cmake-build-debug/catkin_generated/setup_cached.sh
+++ src/cmake-build-debug/catkin_generated/setup_cached.sh
@@ -8,8 +8,7 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/yangzt/yangzt_ws/src/cmake-build-debug/devel:$CMAKE_PREFIX_PATH"
-export LD_LIBRARY_PATH='/home/yangzt/yangzt_ws/src/cmake-build-debug/devel/lib:/home/yangzt/amz_driverless/fsd_skeleton/devel/lib:/home/yangzt/baxtersim_ws/devel/lib:/home/yangzt/baxter_ws/devel/lib:/home/yangzt/yangzt_ws/devel/lib:/opt/ros/kinetic/lib:/opt/ros/kinetic/lib/x86_64-linux-gnu:/home/yangzt/Qt5.12.9/gcc_64/lib:/usr/local/cuda-10.1/lib64'
-export PATH='/opt/ros/kinetic/bin:/home/yangzt/Qt5.12.9/Tools/QtCreator/bin:/home/yangzt/Qt5.12.9/5.12.9/gcc_64/bin:/home/yangzt/bin:/home/yangzt/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/usr/local/cuda-10.1/bin'
+export LD_LIBRARY_PATH="/home/yangzt/yangzt_ws/src/cmake-build-debug/devel/lib:$LD_LIBRARY_PATH"
 export PKG_CONFIG_PATH="/home/yangzt/yangzt_ws/src/cmake-build-debug/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PYTHONPATH="/home/yangzt/yangzt_ws/src/cmake-build-debug/devel/lib/python2.7/dist-packages:$PYTHONPATH"
 export ROSLISP_PACKAGE_DIRECTORIES="/home/yangzt/yangzt_ws/src/cmake-build-debug/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
